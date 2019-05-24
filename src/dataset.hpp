4c5b42d1ba0a353bb238716506d56cbd05408c42
tevador <tevador@gmail.com>
2019-05-24 19:29:21 +0200

    Fix MSVC debug compilation

--- src/dataset.hpp
+++ src/dataset.hpp
@@ -54,7 +54,9 @@ struct randomx_cache {
 
 //A pointer to a standard-layout struct object points to its initial member
 static_assert(std::is_standard_layout<randomx_dataset>(), "randomx_dataset must be a standard-layout struct");
+#ifndef _DEBUG
 static_assert(std::is_standard_layout<randomx_cache>(), "randomx_cache must be a standard-layout struct");
+#endif
 
 namespace randomx {
 
